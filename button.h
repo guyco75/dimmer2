bf89280830f4ab07b0acacc3b9f248b67f756b97
guyco75 <x>
2017-11-18 19:02:49 +0200

    move button functionality to button.h
    Signed-off-by: guyco75 <x>

---
+++ button.h
@@ -0,0 +1,43 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#define DEBOUNCE_INTERVAL_MS (70)             // Unstable/debounce duration
+
+struct button {
+  unsigned long last_change_ms;
+  uint8_t pin;
+  bool state;
+  bool unstable_state;
+
+  void setup(uint8_t pin) {
+    this->pin = pin;
+    state = unstable_state = 0;
+    last_change_ms = millis();
+  }
+
+  bool read_state() {
+    bool read_val = !(PINC & _BV(pin));
+
+    if (read_val != unstable_state) {
+#ifdef DIMMER_DEBBUG
+      if (read_val == state) {
+        Serial.print("unstable change  ");
+        Serial.println(millis() - last_change_ms);
+      }
+#endif
+      unstable_state = read_val;
+      last_change_ms = millis();
+    } else {
+      if (read_val != state && (millis() - last_change_ms > DEBOUNCE_INTERVAL_MS)) {
+        state = read_val;
+#ifdef DIMMER_DEBBUG
+        Serial.print("stable change ************  ");
+        Serial.println(state?"ON":"OFF");
+#endif
+      }
+    }
+    return state;
+  }
+};
+
+#endif
--- dimmer2.ino
+++ dimmer2.ino
@@ -1,6 +1,6 @@
 #include "serial_parser.h"
+#include "button.h"
 
-#define DEBOUNCE_INTERVAL_MS (70)             // Unstable/debounce duration
 #define BTN_LONG_PRESS_THRESHOLD_MS (500)     // Below that it's considered a SHORT press; above it a LONG press
 
 #define LOOP_DURATION (10)
@@ -28,43 +28,7 @@ do { \
 
 #endif
 
-struct button {
-  unsigned long last_change_ms;
-  uint8_t pin;
-  bool state;
-  bool unstable_state;
-
-  void setup(uint8_t pin) {
-    this->pin = pin;
-    state = unstable_state = 0;
-    last_change_ms = millis();
-  }
-
-  bool read_state() {
-    bool read_val = !(PINC & _BV(pin));
-
-    if (read_val != unstable_state) {
-#ifdef DIMMER_DEBBUG
-      if (read_val == state) {
-        Serial.print("unstable change  ");
-        Serial.println(millis() - last_change_ms);
-      }
-#endif
-      unstable_state = read_val;
-      last_change_ms = millis();
-    } else {
-      if (read_val != state && (millis() - last_change_ms > DEBOUNCE_INTERVAL_MS)) {
-        state = read_val;
-#ifdef DIMMER_DEBBUG
-        Serial.print("stable change ************  ");
-        Serial.println(state?"ON":"OFF");
-#endif
-      }
-    }
-    return state;
-  }
-};
-
+/*****************************************/
 
 enum light_state {
   LIGHT_ST_NO_ACTION,
